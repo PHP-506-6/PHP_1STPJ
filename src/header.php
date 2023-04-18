13dcd1336578b1b5a3c3c0abfaadaf742331f8d0
kbj95 <gwonbongjeong@gmail.com>
2023-04-18 16:26:02 +0900

    0418 header_today 값 입력

--- src/header.php
+++ src/header.php
@@ -1,6 +1,6 @@
 <header>
     <h1><img src="../src/img/logo.png" /></h1>
-    <p class="date">TODAY</p>
+    <p class="date">TODAY <?php echo date("Y-m-d") ?></p>
     <div class="goal_text">
         <form method="">
             <input type="text" maxlength="20" class="goal" />
