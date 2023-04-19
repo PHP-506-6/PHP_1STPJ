9ec458953faac2f328e34b47d1a3c9ca27cda075
awfawf123 <awfawf123@naver.com>
2023-04-19 13:02:21 +0900

    04/19 common삭제 J

--- src/list.php
+++ src/list.php
@@ -1,12 +1,10 @@
 <?php
     define("DOC_ROOT",$_SERVER["DOCUMENT_ROOT"]."/");//root 설정
     define("URL_DB",DOC_ROOT."first_pj/src/common/db_common.php");// db연결
-    define("URL_FUNC",DOC_ROOT."first_pj/src/db_query/db_list01.php"); // function 연결
-    define("URL_HEADER_FUNC",DOC_ROOT."first_pj/src/db_query/db_common.php"); // function 연결
+    define("URL_DB_LIST01",DOC_ROOT."first_pj/src/db_query/db_list01.php"); // function 연결
     define("URL_HEADER",DOC_ROOT."first_pj/src/header.php"); //header 연결
     include_once(URL_DB);
-    include_once(URL_HEADER_FUNC);
-    include_once(URL_FUNC);
+    include_once(URL_DB_LIST01);
 
     $arr_get = $_GET;
     //최초 페이지 열때 페이지 넘버
