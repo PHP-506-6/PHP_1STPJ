9f32867a682dbe85d5f9838fc3f704bed86d387d
miiihyeon9 <miiiihyeon98@gmail.com>
2023-04-19 10:10:35 +0900

    0419 insert css 추가 M

--- src/insert01.php
+++ src/insert01.php
@@ -6,16 +6,16 @@
     include_once(URL_DB);
     include_once(URL_FUNC);
     
-    $result_list = select_obj_list();
-    $http_method = $_SERVER["REQUEST_METHOD"]; 
+    $result_list = select_obj_list();                       //obj_list에서 가장 최근자료 출력
+    $http_method = $_SERVER["REQUEST_METHOD"];              // method 요청
 
-    if ($http_method ==="POST")                                          // POST일때 
+    if ( $http_method === "POST" )                          // POST일때 
     {
         $arr_post = $_POST;
         $result_cnt = insert_list_info( $arr_post );        // DB에 list정보 추가
         
-        header( "Location:list.php" );                      // list페이지  redirect
-        exit();
+        header( "Location:list.php" );                      // list페이지로  redirect
+        exit();                                             // 종료
     }
 
 ?>
@@ -27,44 +27,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert</title>
+    <title>Awake</title>
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/insert01.css">
 </head>
 <body>
     <header>
         <h1><img src="../src/img/logo.png" /></h1>
         <p class="date">TODAY <?php echo date("Y-m-d") ?></p>
         <div class="goal_text">
-            <form>
             <?php echo $result_list["obj_contents"]?>
-            </form>
         </div>
     </header>
+    <div class="container">
+        <div class="line"></div>
+        <form method="post" action="insert01.php">
+            <div class="form_box1">
+                <label for="list_title">제목</label>
+                <input type="text" name="list_title" id="list_title" required maxlength="50">
 
-    <form method="post" action="insert01.php">
-        <label for="list_title">제목</label>
-        <input type="text" name="list_title" required>
+                <label for="list_contents">내용</label>
+                <input type="text" name="list_contents" id="list_contents" required maxlength="50">
 
-        <label for="list_contents">내용</label>
-        <input type="text" name="list_contents" required>
-
-        <label for="ex_set">세트</label>
-        <input type="text" name="ex_set" >
-        <!-- CSS세트 뒤에 after로 SET 적기 -->
-        
-        <label for="ex_num">횟수</label>
-        <input type="text" name="ex_num" >
-        <!-- css세트 뒤에 after로 '회' 적기 -->
-        
-        <div>시간</div>
-        <input type="text" name="ex_hour" >
-        <label for="ex_hour">시간</label>
-        <input type="text" name="ex_min">
-        <label for="ex_min">분</label>
-
-        <button type="submit">작성</button>
-        <button><a href="list.php">취소</a></button>
-    </form>
-
+            </div>
+            <div class="form_box2">
+                
+                    <label class="box2_tit" for="ex_set">세트</label>
+                    <input type="text" name="ex_set" maxlength="2" id="ex_set" >
+                    <span class="set_box">SET</span>
+                
+                    <label class="box2_tit" for="ex_num">횟수</label>
+                    <input type="text" name="ex_num" maxlength="3" id="ex_num" >
+                    <span class="num_box" >회</span>
+                
+                    <span  class="time_lab box2_tit">시간</span>
+                    <input class="time" type="text" name="ex_hour" maxlength="1" id="ex_hour" >
+                    <label for="ex_hour">시간</label>
+                    <input class="time" type="text" name="ex_min" maxlength="2" id="ex_min" >
+                    <label  for="ex_min">분</label>
+            </div>
+            <button class="save_btn btnBlueGreen btnFloat" type="submit">SAVE</button>
+            <button class="cancel_btn btnBlueGreen btnFloat"><a href="list.php">CANCEL</a></button>
+        </form>
+    </div>
 </body>
 </html>
